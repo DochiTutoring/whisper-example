3ba3a8236468cefb3d574ec6054b8739f251b6f0
dochitutoring <dochi@tutoring.co.kr>
2023-09-07 10:43:35 +0900

    [ADD] pip 의존성에 pyannote/speaker-diarization 관련 의존성 추가

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import whisper
+import datetime
 
-# [Reference] https://github.com/openai/whisper
-model_name: str = 'base.en'
-model = whisper.load_model('base.en')
+from stt import predict_by_whisper
 
-audio_filename = 'files/sample-0.mp3'
-result = model.transcribe(audio_filename)
+# whisper (단독으로 3분 36초 음성을 42초만에 변환을 할 수 있음)
+print(datetime.datetime.now())
+whisper_example_result = predict_by_whisper('files/sample-conversation.mp3')
+print(datetime.datetime.now())
 
-print(result['text'])
+print(whisper_example_result)
---
+++ stt/__init__.py
@@ -0,0 +1,17 @@
+import torch.cuda
+import whisper
+
+
+def predict_by_whisper(audio_filename: str) -> str:
+    """
+    [Reference] https://github.com/opeanai/whisper
+
+    :param audio_filename: str
+            오디오 파일의 이름 (확장자 포함)
+    :return: stt predict 결과
+    """
+    model_name: str = 'base.en'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = whisper.load_model(model_name)
+
+    return model.transcribe(audio_filename)['text']
